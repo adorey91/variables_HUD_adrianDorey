98cbd16c7bf3ba3cd9662366f8e171ea9218eca8
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2023-09-18 08:54:41 -0300

    Fixed Code & Changed format

--- variables_HUD_adrianDorey/Program.cs
+++ variables_HUD_adrianDorey/Program.cs
@@ -12,7 +12,7 @@ namespace virablesHUD_v1._0
         static void Main(string[] args)
         {
             //declarations & initializing
-            string game = "Variables"; // game name
+            string game = "Variables_HUD_v1.0"; // game name
             string playerName;
             float score = 0f; // score is 0
             int health = 100; // health is 100
@@ -27,8 +27,10 @@ namespace virablesHUD_v1._0
             playerName = Console.ReadLine();
 
             Console.Clear();
-                       
-            Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+            
+            Console.WriteLine("Game Name: "+game);
+            Console.WriteLine("Player Name: " + playerName);
+            Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier);
             Console.WriteLine(""); 
             Console.WriteLine(playerName+" walks through forest and falls in a hole and takes 25 damage");
             Console.WriteLine("");
@@ -40,7 +42,9 @@ namespace virablesHUD_v1._0
             Console.ReadKey(true);
             Console.Clear();
 
-            Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+            Console.WriteLine("Game Name: " + game);
+            Console.WriteLine("Player Name: " + playerName);
+            Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
             Console.WriteLine("");
             Console.WriteLine("Half way through the tunnel, " + playerName + " sees a dark figure that looks like it could be an enemy. They find a rock and throw it at the enemy.");
             Console.WriteLine("");
@@ -49,7 +53,9 @@ namespace virablesHUD_v1._0
             Console.ReadKey(true);
             Console.Clear();
 
-            Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+            Console.WriteLine("Game Name: " + game);
+            Console.WriteLine("Player Name: " + playerName);
+            Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
             Console.WriteLine("");
             Console.WriteLine("The dark figure quickly approaches and swallows "+playerName+" whole");
             Console.WriteLine("");
@@ -62,7 +68,9 @@ namespace virablesHUD_v1._0
             Console.ReadKey(true);
             Console.Clear();
 
-            Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+            Console.WriteLine("Game Name: " + game);
+            Console.WriteLine("Player Name: " + playerName);
+            Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
             Console.WriteLine("");
             Console.WriteLine(playerName+" spawns back at the bottom of the tunnel");
             Console.WriteLine();
@@ -73,7 +81,9 @@ namespace virablesHUD_v1._0
             Console.Clear();
 
 
-            Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+            Console.WriteLine("Game Name: " + game);
+            Console.WriteLine("Player Name: " + playerName);
+            Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
             Console.WriteLine("");
             Console.WriteLine("They follow the same tunnel as before but this time makes it to the end without seeing the dark figure. The tunnel leads to a beautfiul apple orchard. Hero decides to eat an apple. The apple gives them 50 health");
             Console.WriteLine("");
@@ -85,7 +95,9 @@ namespace virablesHUD_v1._0
             Console.ReadKey(true);
             Console.Clear();
 
-            Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+            Console.WriteLine("Game Name: " + game);
+            Console.WriteLine("Player Name: " + playerName);
+            Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
             Console.WriteLine("");
             Console.WriteLine("After healing " + playerName+ " goes for a walk through the orchard where they find a chest and decide to open it.");
             Console.WriteLine("");
@@ -94,7 +106,9 @@ namespace virablesHUD_v1._0
             Console.ReadKey(true);
             Console.Clear();
 
-            Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+            Console.WriteLine("Game Name: " + game);
+            Console.WriteLine("Player Name: " + playerName);
+            Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
             Console.WriteLine("");
             Console.WriteLine(playerName+" finds a map to the exit and money but the inside of the lid to the chest has a sign that says only pick one.");
             Console.WriteLine("");
@@ -103,7 +117,9 @@ namespace virablesHUD_v1._0
             Console.ReadKey(true);
             Console.Clear();
 
-            Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+            Console.WriteLine("Game Name: " + game);
+            Console.WriteLine("Player Name: " + playerName);
+            Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
             Console.WriteLine("");
             Console.WriteLine("Type "+playerName+"'s direction... map, money or both");
             Console.WriteLine("");
@@ -115,7 +131,9 @@ namespace virablesHUD_v1._0
 
             if (playerInput == "map")
             {
-                Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+                Console.WriteLine("Game Name: " + game);
+                Console.WriteLine("Player Name: " + playerName);
+                Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
                 Console.WriteLine("");
                 Console.WriteLine("Using the map, "+playerName+" finds their way out of the orchard and back home where there's 1000$ and a pie waiting");
                 Console.WriteLine("");
@@ -126,14 +144,18 @@ namespace virablesHUD_v1._0
                 Console.ReadKey(true);
                 Console.Clear();
 
-                Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+                Console.WriteLine("Game Name: " + game);
+                Console.WriteLine("Player Name: " + playerName);
+                Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
                 Console.WriteLine("");
                 Console.WriteLine(playerName+" lives happily ever after.");
                 Console.WriteLine("");
             }
             else if (playerInput == "money")
             {
-                Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+                Console.WriteLine("Game Name: " + game);
+                Console.WriteLine("Player Name: " + playerName);
+                Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
                 Console.WriteLine("");
                 Console.WriteLine(playerName+" has taken the money and receives 2500 but will not find their way back home but instead builds a new home in the orchard.");
                 Console.WriteLine("");
@@ -143,14 +165,18 @@ namespace virablesHUD_v1._0
                 Console.ReadKey(true);
                 Console.Clear();
 
-                Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+                Console.WriteLine("Game Name: " + game);
+                Console.WriteLine("Player Name: " + playerName);
+                Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
                 Console.WriteLine("");
                 Console.WriteLine(playerName+" lives a pretty good life.");
                 Console.WriteLine("");
             }
             else if (playerInput == "both")
             {
-                Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+                Console.WriteLine("Game Name: " + game);
+                Console.WriteLine("Player Name: " + playerName);
+                Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
                 Console.WriteLine("");
                 Console.WriteLine(playerName+" being gready decides to take both. The dark figure appears from no where, screams and swallows them whole once more, but this time takes all remaining lives and health.");
                 Console.WriteLine("");
@@ -163,13 +189,17 @@ namespace virablesHUD_v1._0
                 score = score - 100;
                 lives = lives - lives;
 
-                Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+                Console.WriteLine("Game Name: " + game);
+                Console.WriteLine("Player Name: " + playerName);
+                Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
                 Console.WriteLine("");
 
             }
             else
             {
-                Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+                Console.WriteLine("Game Name: " + game);
+                Console.WriteLine("Player Name: " + playerName);
+                Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
                 Console.WriteLine("");
                 Console.WriteLine(playerName+" Dies.");
                 Console.WriteLine("You didn't listen.");
@@ -183,7 +213,9 @@ namespace virablesHUD_v1._0
                 Console.Clear();
 
 
-                Console.WriteLine("Game Name: " + game + "| " + "Player Name: " + playerName + "| " + "Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier));
+                Console.WriteLine("Game Name: " + game);
+                Console.WriteLine("Player Name: " + playerName);
+                Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier); 
                 Console.WriteLine("");
             }
 
